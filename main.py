56ee94b61bef2de43418c3354f446d212c005248
Serg Sheva <youremail@example.com>
2025-07-05 08:31:35 +0300

    додано старт

--- main.py
+++ main.py
@@ -252,3 +252,8 @@ async def confirm_ride_end(user_id: str):
 
     with path.open("w") as f:
         json.dump(ratings, f, indent=4)
+
+
+        if __name__ == "__main__":
+    import asyncio
+    asyncio.run(dp.start_polling(bot))
